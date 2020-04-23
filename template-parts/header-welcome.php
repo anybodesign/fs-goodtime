62bbcbb3fc8250156f46af4d595c1bace113aa20
Thomas Villain <atelier@anybodesign.com>
2020-04-23 14:28:33 +0200

    Display welcome default

--- template-parts/header-welcome.php
+++ template-parts/header-welcome.php
@@ -1,7 +1,7 @@
 <?php if ( !defined('ABSPATH') ) die(); 
 
-	$h_title = get_theme_mod('welcome_title');
-	$h_text = get_theme_mod('welcome_text');	
+	$h_title = get_theme_mod('welcome_title', __('Aloha :)', 'fs-goodtime') );
+	$h_text = get_theme_mod('welcome_text', __('Welcome and have a good time', 'fs-goodtime') );	
 	$scroll = get_theme_mod('scrolldown');
 ?>
 
@@ -9,17 +9,13 @@
 			
 			<div class="fs-welcome">
 				
-				<?php if ($h_title) { ?>
 				<p class="fs-welcome-title h1-like">
 					<?php echo $h_title; ?>
 				</p>
-				<?php } ?>
 				
-				<?php if ($h_text) { ?>
 				<p class="fs-welcome-text text-intro">
 					<?php echo $h_text; ?>
 				</p>
-				<?php } ?>
 				
 				<?php if ( $scroll != false ) { ?> 
 				<button class="scroll-down">
