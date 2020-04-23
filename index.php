88b8aa8bcec437422d63df73c2d292c7171a46cc
Thomas Villain <atelier@anybodesign.com>
2020-04-23 14:28:20 +0200

    HTML/CSS bugfix

--- index.php
+++ index.php
@@ -14,9 +14,10 @@ get_header(); ?>
 				<div class="page-wrap has-sidebar">
 					
 					<div class="page-content">
-					<?php 
+					
+					<?php if ( get_theme_mod('layout_option') == 'version2' || get_theme_mod('layout_option') == 'version3' ) {
 						get_template_part( 'template-parts/page', 'title' ); 
-					?>
+					} ?>
 
 					<?php if ( have_posts() ) : ?>		
 			
