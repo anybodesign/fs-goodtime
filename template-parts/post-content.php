be92cdf66ab9d8857ba0edcd0edaee2b45adae65
Thomas Villain <atelier@anybodesign.com>
2020-05-25 12:46:16 +0200

    Display title on single (header v2 & v3)

--- template-parts/post-content.php
+++ template-parts/post-content.php
@@ -14,7 +14,12 @@
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 						<div class="post-content">
-							<?php the_content(); ?>
+							<?php 
+								if ( get_theme_mod('layout_option') == 'version2' || get_theme_mod('layout_option') == 'version3' ) {
+									get_template_part( 'template-parts/page', 'title' );
+								} 
+								the_content();
+							?>
 						</div>
 						<?php get_template_part('template-parts/post', 'meta'); ?>
 
